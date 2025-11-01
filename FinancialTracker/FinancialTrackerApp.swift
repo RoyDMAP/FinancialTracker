951f2b6dbe7821452bd89a9b17d37883899a9398
RoyDMAP <roy.dimapilis@yahoo.com>
2025-11-01 11:03:42 -0700

    Initial Commit

---
+++ FinancialTracker/FinancialTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialTrackerApp.swift
+//  FinancialTracker
+//
+//  Created by Roy Dimapilis on 11/1/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancialTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
